bc60b6de07e880f06120668a7e1b7c7e04aa3f0a
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2026-02-10 18:39:05 -0800

    global async handlers

--- services/src/routes/user.routes.ts
+++ services/src/routes/user.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
+import { asyncHandler } from "../middleware/async-handler.js";
 
 const userRouter = Router();
 
-userRouter.get("/anurag", (_, res) => {
-  return res.json({
-    message: {
-      name: "Anurag",
-      age: 27,
-    },
-  });
-});
+userRouter.get(
+  "/anurag",
+  asyncHandler(async (_, res) => {
+    return res.json({
+      message: {
+        name: "Anurag",
+        age: 27,
+      },
+    });
+  })
+);
 
 export default userRouter;
